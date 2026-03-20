b4d19e9872bc3980f3a7381898491eed53006a87
markDev77 <marcomorales.morales.77@gmail.com>
2026-03-20 15:01:30 -0600

    Update server.js

--- server.js
+++ server.js
@@ -766,12 +766,6 @@ function getShopQueue(shop) {
 }
 
 function enqueueShopJob(shop, jobName, fn) {
-
-  if (isDuplicateExecution(shop, productId)) {
-  log("DUPLICATE EXECUTION BLOCKED", { shop, productId });
-  return;
-}
-
   const normalizedShop = normalizeShopDomain(shop);
   const q = getShopQueue(normalizedShop);
   const jobId = `${Date.now()}-${Math.random().toString(16).slice(2)}`;
