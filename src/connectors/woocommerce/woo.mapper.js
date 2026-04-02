6fdd6a135f1bb2a589e1ad4f6a274a0e55cac582
markDev77 <marcomorales.morales.77@gmail.com>
2026-04-01 18:09:30 -0600

    Update woo.mapper.js

--- src/connectors/woocommerce/woo.mapper.js
+++ src/connectors/woocommerce/woo.mapper.js
@@ -1,1 +1,26 @@
+function mapWooToZeus(product) {
+  return {
+    input: {
+      title: product.name,
+      description: product.description,
+    },
+    taxonomy: {
+      categories: product.categories || []
+    }
+  };
+}
 
+function mapZeusToWoo(aiResult, originalProduct) {
+  return {
+    name: aiResult.title || originalProduct.name,
+    description: aiResult.description || originalProduct.description,
+    categories: (originalProduct.categories || []).map(c => ({
+      id: c.id
+    }))
+  };
+}
+
+module.exports = {
+  mapWooToZeus,
+  mapZeusToWoo,
+};
