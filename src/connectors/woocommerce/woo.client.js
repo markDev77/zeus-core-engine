2a724fa5419746d8084f18a98d3d283cdf0213ce
markDev77 <marcomorales.morales.77@gmail.com>
2026-04-01 18:11:43 -0600

    Update woo.client.js

--- src/connectors/woocommerce/woo.client.js
+++ src/connectors/woocommerce/woo.client.js
@@ -2,18 +2,23 @@ const axios = require("axios");
 
 const BASE_URL = "https://lo-tengo.com.mx/wp-json/wc/v3";
 
-const auth = {
-  username: "ck_1f2ee08f6224be07d8f96a8cf2d72bc6be902f2e",
-  password: "cs_18fa66ba79f1edaa0405cf8f763a6e86ee931519",
-};
+// 🔥 USAR QUERY AUTH (NO BASIC AUTH)
+const CONSUMER_KEY = "ck_1f2ee08f6224be07d8f96a8cf2d72bc6be902f2e";
+const CONSUMER_SECRET = "cs_18fa66ba79f1edaa0405cf8f763a6e86ee931519";
+
+function buildUrl(path) {
+  return `${BASE_URL}${path}?consumer_key=${CONSUMER_KEY}&consumer_secret=${CONSUMER_SECRET}`;
+}
 
 async function getProduct(productId) {
-  const res = await axios.get(`${BASE_URL}/products/${productId}`, { auth });
+  const url = buildUrl(`/products/${productId}`);
+  const res = await axios.get(url);
   return res.data;
 }
 
 async function updateProduct(productId, payload) {
-  const res = await axios.put(`${BASE_URL}/products/${productId}`, payload, { auth });
+  const url = buildUrl(`/products/${productId}`);
+  const res = await axios.put(url, payload);
   return res.data;
 }
 
