be37742c441f52e4a01c859b4ccc683c38002150
markDev77 <marcomorales.morales.77@gmail.com>
2026-04-01 18:09:11 -0600

    Update woo.client.js

--- src/connectors/woocommerce/woo.client.js
+++ src/connectors/woocommerce/woo.client.js
@@ -1,1 +1,23 @@
-.
+const axios = require("axios");
+
+const BASE_URL = "https://lo-tengo.com.mx/wp-json/wc/v3";
+
+const auth = {
+  username: "ck_1f2ee08f6224be07d8f96a8cf2d72bc6be902f2e",
+  password: "cs_18fa66ba79f1edaa0405cf8f763a6e86ee931519",
+};
+
+async function getProduct(productId) {
+  const res = await axios.get(`${BASE_URL}/products/${productId}`, { auth });
+  return res.data;
+}
+
+async function updateProduct(productId, payload) {
+  const res = await axios.put(`${BASE_URL}/products/${productId}`, payload, { auth });
+  return res.data;
+}
+
+module.exports = {
+  getProduct,
+  updateProduct,
+};
