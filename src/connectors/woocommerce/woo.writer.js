22171534aea786fa477b15460af5eb0a3de66ba2
markDev77 <marcomorales.morales.77@gmail.com>
2026-04-01 18:09:54 -0600

    Update woo.writer.js

--- src/connectors/woocommerce/woo.writer.js
+++ src/connectors/woocommerce/woo.writer.js
@@ -1,1 +1,9 @@
+const { updateProduct } = require("./woo.client");
 
+async function writeWooProduct(productId, payload) {
+  return await updateProduct(productId, payload);
+}
+
+module.exports = {
+  writeWooProduct,
+};
