d35b2b92d413bb928e41c3dd22b7fd92f4227cb0
markDev77 <marcomorales.morales.77@gmail.com>
2026-04-11 10:54:49 -0600

    Update index.js

--- zeus-v2/src/app/index.js
+++ zeus-v2/src/app/index.js
@@ -1,1 +1,11 @@
+// Nuevo entrypoint oficial ZEUS v2
 
+const orchestration = require('../orchestration');
+
+function runApp(input) {
+    return orchestration(input);
+}
+
+module.exports = {
+    runApp
+};
