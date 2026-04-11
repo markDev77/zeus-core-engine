9af814af41996a860dac31b99d8812b9c646ee6d
markDev77 <marcomorales.morales.77@gmail.com>
2026-04-11 12:05:04 -0600

    Update index.js

--- zeus-v2/src/core/index.js
+++ zeus-v2/src/core/index.js
@@ -1,11 +1,15 @@
 const runTitleEngine = require('./title/title-engine');
+const runDescriptionEngine = require('./description/description-engine');
 
 function runCore(input) {
 
     let output = { ...input };
 
-    // TITLE ENGINE (primer módulo real)
+    // TITLE
     output = runTitleEngine(output);
+
+    // DESCRIPTION
+    output = runDescriptionEngine(output);
 
     return output;
 }
