08cc03c29c6ce6190d098543ed04d0e2a61ec844
markDev77 <marcomorales.morales.77@gmail.com>
2026-04-11 12:08:15 -0600

    Create product-signature.js

---
+++ zeus-v2/src/core/normalizer/product-signature.js
@@ -0,0 +1,30 @@
+function generateSignature(text) {
+
+    if (!text) return '';
+
+    let hash = 0;
+
+    for (let i = 0; i < text.length; i++) {
+        hash = ((hash << 5) - hash) + text.charCodeAt(i);
+        hash |= 0; // convertir a 32bit
+    }
+
+    return hash.toString();
+}
+
+function runProductSignature(input) {
+
+    const normalized_title = input.core?.normalized_title || '';
+
+    const product_signature = generateSignature(normalized_title);
+
+    return {
+        ...input,
+        core: {
+            ...input.core,
+            product_signature
+        }
+    };
+}
+
+module.exports = runProductSignature;
