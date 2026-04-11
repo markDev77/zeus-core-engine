59258a98213f19e02f42dabd4f176d1a38bed0d2
markDev77 <marcomorales.morales.77@gmail.com>
2026-04-11 12:57:49 -0600

    Create title.types.js

---
+++ zeus-v2/src/core/title/title.types.js
@@ -0,0 +1,19 @@
+// title.types.js
+// Definición estructural (no ejecuta lógica)
+
+/**
+ * @typedef {Object} TitleInput
+ * @property {string} title
+ */
+
+/**
+ * @typedef {Object} TitleContext
+ * @property {string} language
+ * @property {string} platform
+ */
+
+/**
+ * @typedef {string} TitleOutput
+ */
+
+module.exports = {};
