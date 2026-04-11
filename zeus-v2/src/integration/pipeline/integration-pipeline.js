9363e201335ecf24b4d024ffa80aa57fe578633d
markDev77 <marcomorales.morales.77@gmail.com>
2026-04-10 23:08:52 -0600

    Update integration-pipeline.js

--- zeus-v2/src/integration/pipeline/integration-pipeline.js
+++ zeus-v2/src/integration/pipeline/integration-pipeline.js
@@ -1,1 +1,17 @@
+const { runValidation } = require('../adapters/validation.adapter');
+const { runExecution } = require('../adapters/execution.adapter');
+const { runObservability } = require('../adapters/observability.adapter');
 
+function runIntegrationPipeline(input) {
+    const validationResult = runValidation(input);
+
+    const executionState = runExecution(validationResult);
+
+    const observabilityOutput = runObservability(executionState);
+
+    return observabilityOutput;
+}
+
+module.exports = {
+    runIntegrationPipeline
+};
