b6daac18a1d08021bcd979c03ad9dd3c641b49f9
markDev77 <marcomorales.morales.77@gmail.com>
2026-04-10 23:07:26 -0600

    Update execution.adapter.js

--- zeus-v2/src/integration/adapters/execution.adapter.js
+++ zeus-v2/src/integration/adapters/execution.adapter.js
@@ -1,1 +1,9 @@
+const runExecutionModule = require('../../execution');
 
+function runExecution(validatedInput) {
+    return runExecutionModule(validatedInput);
+}
+
+module.exports = {
+    runExecution
+};
