0151db3e3cb87e149f7bc7e135ee5a3b4eae9611
markDev77 <marcomorales.morales.77@gmail.com>
2026-04-11 11:01:46 -0600

    Update index.js

--- zeus-v2/src/orchestration/index.js
+++ zeus-v2/src/orchestration/index.js
@@ -1,1 +1,9 @@
+const triggerExecution = require('./execution/execution-trigger');
 
+function runOrchestration(input) {
+    return triggerExecution(input);
+}
+
+module.exports = {
+    runOrchestration
+};
